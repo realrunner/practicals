4f32406129305dbee432b79645bffa037c022794
Mike Nelson <mike@graphiteconnect.com>
2021-11-17 15:36:22 -0700

    Mike's solution

--- ts/src/p1.ts
+++ ts/src/p1.ts
@@ -1,11 +1,47 @@
 export function isPalindrome(s: string): boolean {
-    return false;
+    s = s.toLocaleLowerCase();
+    for (let i = 0; i < s.length / 2; i++) {
+        if (s[i] !== s[s.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 export function reverseWordsInString(s: string): string {
-    return s;
+    return s.split(" ").reverse().join(" ");
 }
 
 export function doCalculation(s: string): number {
-    return 0;
+    const stack: string[] = [];
+    let operand = "";
+    
+    for (let i = 0; i < s.length; i++) {
+        const c = s.charAt(i);
+        if (c === "+" || c === "-") {
+            if (!operand) {
+                throw new Error(`invalid state: operator ${c} without a prior operand in ${s}`);
+            }
+            stack.push(operand);
+            stack.push(c);
+            operand = "";
+        } else {
+            operand += c;
+        }
+    }
+    stack.push(operand);
+
+    while(stack.length >= 3) {
+        const left = parseFloat(stack.shift());
+        const op = stack.shift();
+        const right = parseFloat(stack.shift());
+        let res: number = 0;
+        if (op === "+") {
+            res = left + right;
+        } else if (op === "-") {
+            res = left - right;
+        }
+        stack.unshift(res + "");
+    }
+    return parseFloat(stack[0]);
 }
--- ts/src/p2.ts
+++ ts/src/p2.ts
@@ -1,9 +1,33 @@
 import { HairColor, Person } from "./models";
 
+function ancestors(p: Person): Person[] {
+    return [
+        ...(p.father ? [p.father, ...ancestors(p.father)] : []),
+        ...(p.mother ? [p.mother, ...ancestors(p.mother)] : []),
+    ]
+}
+
 export function namesOfAncestors(p: Person): string[] {
-    return [];
+    return ancestors(p).map(a => a.name);
 }
 
 export function mostCommonHairColorOfAncestors(p: Person): HairColor {
-    return HairColor.BLACK;
+    const hairs = ancestors(p).map(a => a.hairColor);
+    const histogram: {[key: string]: number} = {};
+    hairs.forEach(h => {
+        if (!histogram[h]) {
+            histogram[h] = 0;
+        }
+        histogram[h] += 1;
+    });
+    
+
+    const histoKeys = Object.keys(histogram) as HairColor[];
+    let best: HairColor = histoKeys[0];
+    for (const color of Object.keys(histogram)) {
+        if (histogram[color] > histogram[best]) {
+            best = color as HairColor;
+        }
+    }
+    return best;
 }
