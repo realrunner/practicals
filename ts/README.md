04a2bde2d229aa8cb3aa615d4aa7c9371ebd4339
Mike Nelson <mike@realrunners.com>
2024-04-17 11:26:51 -0600

    clarifications

--- ts/README.md
+++ ts/README.md
@@ -1,6 +1,14 @@
-TypeScript Practical Tests
+## TypeScript Practical Tests
 
 ```
 $ npm install
 $ npm test
+```
+
+## You can run individual tests with a test name pattern
+
+```
+$ npm test -- -t palindrome
+$ npm test -- -t reverse
+$ npm test -- -t common
 ```
--- ts/src/p1.ts
+++ ts/src/p1.ts
@@ -1,11 +1,13 @@
 export function isPalindrome(s: string): boolean {
-    return false;
+  return false;
 }
 
 export function reverseWordsInString(s: string): string {
-    return s;
+  return s;
 }
 
+// note: You can solve this with `return eval(s)` but you shouldn't, that's not the point!
+// Also, using eval in production code is generally bad practice.
 export function doCalculation(s: string): number {
-    return 0;
+  return 0;
 }
